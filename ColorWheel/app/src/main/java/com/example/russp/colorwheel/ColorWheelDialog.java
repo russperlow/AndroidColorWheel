eea462753466884c5a5b1420f88c036c3789332d
rperlow <rperlow@redcom.com>
2017-08-03 16:42:00 -0400

    Dialog displays 6 colors, needs to blend the rest and be optimized

--- ColorWheel/app/src/main/java/com/example/russp/colorwheel/ColorWheelDialog.java
+++ ColorWheel/app/src/main/java/com/example/russp/colorwheel/ColorWheelDialog.java
@@ -6,9 +6,12 @@ import android.app.DialogFragment;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -23,109 +26,185 @@ import android.widget.ProgressBar;
  * Created by russp on 8/2/2017.
  */
 
-public class ColorWheelDialog extends DialogFragment {
+public class ColorWheelDialog extends Dialog {
 
+    public interface OnColorChangedListener{
+        void colorChanged(int color);
+    }
 
-    static Bitmap bitmap;
-    static Context context;
+    private OnColorChangedListener listener;
+    private int initialColor;
 
-    public ColorWheelDialog(){
-
+    public ColorWheelDialog(Context context, OnColorChangedListener listener, int initialColor){
+        super(context);
+        this.listener = listener;
+        this.initialColor = initialColor;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        OnColorChangedListener colorChangedListener = new OnColorChangedListener() {
+            @Override
+            public void colorChanged(int color) {
+                listener.colorChanged(color);
+                dismiss();
+            }
+        };
+        setContentView(new ColorWheelView(getContext(), colorChangedListener, initialColor));
+        setTitle("Choose a Tag Color");
     }
 
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState){
-        final Activity activity = getActivity();
-    }
+    private static class ColorWheelView extends View
+    {
+        static Context context;
+        static int radius = 360;
+        static Paint selectedColor;
+        static int selectedColorInt;
+        private OnColorChangedListener listener;
 
-    public void drawCanvas(){
-        bitmap = Bitmap.createBitmap(50, 50, Bitmap.Config.ARGB_8888);
+        ColorWheelView(Context context, OnColorChangedListener listener, int color){
+            super(context);
+            this.listener = listener;
+            selectedColor = new Paint(Paint.ANTI_ALIAS_FLAG);
+            selectedColor.setColor(0);
+        }
 
-        int radius = 25;
-        for(int x = -radius; x < radius; x++){
-            for(int y = -radius; y < radius; y++){
-                int distance = (int)Math.sqrt(x*x+y*y);
-                if(distance < radius){
-                    int rowLength = 2 * radius;
-                    int adjustX = x + radius;
-                    int adjustY = y + radius;
-                    int pixelWidth = 4;
-                    int index = (adjustX + (adjustY * rowLength)) * pixelWidth;
-                    int angle = (int)Math.atan(y/x);
-                    bitmap.setPixel(x, y, getColor(angle, distance/radius, 1));
+        @Override
+        protected void onDraw(Canvas canvas){
+            canvas.translate(radius, radius);
+            drawCircle(canvas);
+            selectedColor.setColor(selectedColorInt);
+            canvas.drawCircle(canvas.getWidth() - radius - 100, canvas.getHeight() - radius - 100, 75, selectedColor);
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event){
+            float x = event.getX() - radius;
+            float y = event.getY() - radius;
+            float distance = getDistance(x, y);
+
+            int hue = getAngle(x, y);
+            selectedColor.setStyle(Paint.Style.FILL);
+            selectedColor.setStrokeWidth(5);
+            selectedColor.setColor(Color.BLACK);
+//            selectedColorInt = getColor(hue, distance / radius, 1);
+            invalidate();
+            return true;
+        }
+
+        @Override
+        protected void onMeasure(int widthMeasureSpec, int heightMesaureSpect){
+            setMeasuredDimension((int)(radius * 2.5), (int)(radius * 2.5));
+        }
+
+        public void drawCircle(Canvas canvas) {
+            Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+            for (int x = -radius; x < radius; x++) {
+                for (int y = -radius; y < radius; y++) {
+                    // The distance from the center
+                    float distance = getDistance(x, y);
+
+                    // Skip points outside of the circle
+                    if(distance > radius)
+                        continue;
+
+                    int hue = getAngle(x, y);
+                    double saturation = distance / radius;
+
+                    paint.setColor(getColor(hue, (float)saturation, 1));
+                    paint.setStrokeWidth(20);
+                    paint.setStyle(Paint.Style.FILL);
+
+//                    canvas.drawLine(0, 0, x, y, paint);
+                    canvas.drawPoint(x, y, paint);
+
                 }
             }
         }
+
+        /**
+         * Get the length of the vector (x, y)
+         *
+         * @param x how far in the x direction it goes
+         * @param y how far in the y direction it goes
+         *
+         * @return the distance the vector goes
+         */
+        public float getDistance(float x, float y){
+            return (float)Math.sqrt((x * x) + (y * y));
+        }
+
+        /**
+         * Get the angle of a given point from (0, 0)
+         *
+         * @param x coordinate
+         * @param y coordinate
+         *
+         * @return angle of a given point in degrees
+         */
+        public int getAngle(float x, float y){
+            double angle = Math.atan2(y, x);
+            return (int)(((angle + Math.PI) / (2 * Math.PI)) * 360);
+        }
+
+        /**
+         * Get the HSV color (formulas from Wikipedia HSL and HSV)
+         * and convert to rgb then to int
+         *
+         * @param hue        the angle of this colors location
+         * @param saturation distance from the center
+         * @param value      always 1 for now
+         * @return the decimal value of a color
+         */
+        public int getColor(int hue, float saturation, float value) {
+            // Calculate the values to assign
+            float chroma = value * saturation;
+
+            // Get hue prime
+            hue = Math.abs(hue / 60);
+            float x = chroma * (1 - Math.abs((hue % 2) - 1));
+
+            // Set all to 0 (black) by default
+            float r1 = 0;
+            float g1 = 0;
+            float b1 = 0;
+
+            if (hue >= 0 && hue <= 1) {
+                r1 = chroma;
+                g1 = x;
+                b1 = 0;
+            } else if (hue >= 1 && hue <= 2) {
+                r1 = x;
+                g1 = chroma;
+                b1 = 0;
+            } else if (hue >= 2 && hue <= 3) {
+                r1 = 0;
+                g1 = chroma;
+                b1 = x;
+            } else if (hue >= 3 && hue <= 4) {
+                r1 = 0;
+                g1 = x;
+                b1 = chroma;
+            } else if (hue >= 4 && hue <= 5) {
+                r1 = x;
+                g1 = 0;
+                b1 = chroma;
+            } else if (hue >= 5 && hue <= 6) {
+                r1 = chroma;
+                g1 = 0;
+                b1 = x;
+            }
+
+            float m = value - chroma;
+
+            // Calculate RGB values and convert from decimals
+            int red = (int) (r1 + m) * 255;
+            int green = (int) (g1 + m) * 255;
+            int blue = (int) (b1 + m) * 255;
+
+            return Color.argb(1, red, green, blue);
+        }
     }
-
-    /**
-     * Get the HSV color (formulas from Wikipedia HSL and HSV)
-     * and convert to rgb then to int
-     *
-     * @param hue the angle of this colors location
-     * @param saturation distance from the center
-     * @param value always 1 for now
-     *
-     * @return the decimal value of a color
-     */
-    public int getColor(int hue, float saturation, float value){
-        // Calculate the values to assign
-        float chroma = value * saturation;
-        float x = chroma * (1 - Math.abs((hue % 2) - 1));
-
-        // Get hue prime
-        hue = hue / 60;
-
-        // Set all to 0 (black) by default
-        float r1 = 0;
-        float g1 = 0;
-        float b1 = 0;
-
-        if(hue >= 0 && hue <= 1)
-        {
-            r1 = chroma;
-            g1 = x;
-            b1 = 0;
-        }
-        else if(hue >= 1 && hue <= 2){
-            r1 = x;
-            g1 = chroma;
-            b1 = 0;
-        }
-        else if(hue >= 2 && hue <= 3){
-            r1 = 0;
-            g1 = chroma;
-            b1 = x;
-        }
-        else if(hue >= 3 && hue <= 4){
-            r1 = 0;
-            g1 = x;
-            b1 = chroma;
-        }
-        else if(hue >= 4 && hue <= 5){
-            r1 = x;
-            g1 = 0;
-            b1 = chroma;
-        }
-        else if(hue >= 5 && hue <= 6){
-            r1 = chroma;
-            g1 = 0;
-            b1 = x;
-        }
-
-        float m = value - chroma;
-
-        // Calculate RGB values and convert from decimals
-        int red = (int)(r1+m) * 255;
-        int green = (int)(g1+m) * 255;
-        int blue = (int)(b1+m) * 255;
-
-        return (red * 65536) + (green * 256) + blue;
-    }
-
 }
--- ColorWheel/app/src/main/java/com/example/russp/colorwheel/MainActivity.java
+++ ColorWheel/app/src/main/java/com/example/russp/colorwheel/MainActivity.java
@@ -5,23 +5,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ColorWheelDialog.OnColorChangedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = new Button(this);
-        button.setText("Open Color Wheel");
-        button.setWidth(50);
-        button.setHeight(50);
+        Button button = (Button)findViewById(R.id.color_wheel_button) ;
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                new ColorWheelDialog(MainActivity.this, MainActivity.this, 0).show();
             }
         });
+
+    }
+
+    @Override
+    public void colorChanged(int color) {
 
     }
 }
